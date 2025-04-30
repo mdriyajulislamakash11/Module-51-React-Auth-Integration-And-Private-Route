88c25617f271918faee7173316803c53db9ac5f0
Akash <hafejmohammadakash@gmail.com>
2025-04-30 11:52:51 +0600

    ;oading er kaj korlam

--- src/AuthProviders.jsx
+++ src/AuthProviders.jsx
@@ -1,56 +1,57 @@
-import React, { createContext, useEffect, useState } from 'react';
-import auth from '../fairebase.init';
-import { createUserWithEmailAndPassword, onAuthStateChanged, signInWithEmailAndPassword, signOut } from 'firebase/auth';
+import React, { createContext, useEffect, useState } from "react";
+import auth from "../fairebase.init";
+import {
+  createUserWithEmailAndPassword,
+  onAuthStateChanged,
+  signInWithEmailAndPassword,
+  signOut,
+} from "firebase/auth";
 
-export const AuthContext = createContext(null)
+export const AuthContext = createContext(null);
 
-const AuthProviders = ({children}) => {
-    const[user, setUser]=useState(null)
+const AuthProviders = ({ children }) => {
+  const [user, setUser] = useState(null);
+  const [loading, setLoading] = useState(true);
 
-    const createUser = (email, password) => {
-        return createUserWithEmailAndPassword(auth, email, password)
-    }
+  const createUser = (email, password) => {
+    setLoading(true);
+    return createUserWithEmailAndPassword(auth, email, password);
+  };
 
-    const loginUser = (email, password) => {
-        return signInWithEmailAndPassword(auth, email, password)
-    }
+  const loginUser = (email, password) => {
+    setLoading(true);
+    return signInWithEmailAndPassword(auth, email, password);
+  };
 
-    const logOutUser = () => {
-        return signOut(auth);
-    }
+  const logOutUser = () => {
+    setLoading(true);
 
+    return signOut(auth);
+  };
 
+  // Observer
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
+      console.log("currently login user", currentUser);
+      setUser(currentUser);
+      setLoading(false);
+    });
+    return () => {
+      unsubscribe();
+    };
+  }, []);
 
-    // Observer
-    useEffect(() => {
-        const unsubscribe = onAuthStateChanged(auth, currentUser => {
-            console.log("currently login user", currentUser)
-            setUser(currentUser)
-        })
-        return() => {
-            unsubscribe()
-        }
+  const authInfo = {
+    user,
+    loading,
+    createUser,
+    loginUser,
+    logOutUser,
+  };
 
-
-    }, [])
-
-
-    const authInfo = {
-        name: "akash",
-        user,
-        createUser,
-        loginUser,
-        logOutUser,
-
-    }
-
-
-
-    return (
-        <AuthContext.Provider value={authInfo}>
-            {children}
-        </AuthContext.Provider>
-    );
+  return (
+    <AuthContext.Provider value={authInfo}>{children}</AuthContext.Provider>
+  );
 };
 
 export default AuthProviders;
--- src/components/PrivateRoute.jsx
+++ src/components/PrivateRoute.jsx
@@ -1,19 +1,19 @@
-import React, { useContext } from 'react';
-import { AuthContext } from '../AuthProviders';
-import { Navigate } from 'react-router-dom';
+import React, { useContext } from "react";
+import { AuthContext } from "../AuthProviders";
+import { Navigate } from "react-router-dom";
 
-const PrivateRoute = ({children}) => {
+const PrivateRoute = ({ children }) => {
+  const { user, loading } = useContext(AuthContext);
 
-    const {user} = useContext(AuthContext)
+  if(loading){
+    return <span className="loading loading-bars loading-lg"></span>;
+  }
 
-    if(user){
-        return children;
-    }
+  if (user) {
+    return children;
+  };
 
-
-    return (
-        <Navigate to="/login"></Navigate>
-    );
+  return <Navigate to="/login"></Navigate>;
 };
 
 export default PrivateRoute;
